431465469fbd2180e0d369f008b82fcf8e86c150
zjadzhd <434586852@qq.com>
2023-07-31 11:00:43 +0800

    Fix Context Patch

--- contextpatch.py
+++ contextpatch.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
+import re
 
 
 def scan_context(file) -> dict:  # 读取context文件返回一个字典
     context = {}
-    with open(file, "r") as file_:
+    with open(file, "r", encoding='utf-8') as file_:
         for i in file_.readlines():
-            filepath, *other = i.strip().split()
+            filepath, *other = i.strip().replace('\\', '').split()
             context[filepath] = other
             if len(other) > 1:
                 print(f"Warn:{i[0]} has too much data.")
@@ -15,28 +16,28 @@ def scan_context(file) -> dict:  # 读取context文件返回一个字典
 
 
 def scan_dir(folder) -> list:  # 读取解包的目录，返回一个字典
-    part_name = os.path.dirname(folder)
-    allfiles = ['/', '/lost\\+found', f'/{part_name}/lost\\+found', f'/{part_name}/']
+    part_name = os.path.basename(folder)
+    allfiles = ['/', '/lost+found', f'/{part_name}/lost+found', f'/{part_name}/']
     for root, dirs, files in os.walk(folder, topdown=True):
         for dir_ in dirs:
             if os.name == 'nt':
-                allfiles.append(os.path.join(root, dir_).replace(folder, os.path.basename(folder)).replace('\\', '/'))
+                allfiles.append(os.path.join(root, dir_).replace(folder, '/'+part_name).replace('\\', '/'))
             elif os.name == 'posix':
-                allfiles.append(os.path.join(root, dir_).replace(folder, os.path.basename(folder)))
+                allfiles.append(os.path.join(root, dir_).replace(folder, '/'+part_name))
         for file in files:
             if os.name == 'nt':
-                allfiles.append(os.path.join(root, file).replace(folder, os.path.basename(folder)).replace('\\', '/'))
+                allfiles.append(os.path.join(root, file).replace(folder, '/'+part_name).replace('\\', '/'))
             elif os.name == 'posix':
-                allfiles.append(os.path.join(root, file).replace(folder, os.path.basename(folder)))
+                allfiles.append(os.path.join(root, file).replace(folder, '/'+part_name))
     return sorted(set(allfiles), key=allfiles.index)
 
 
 def context_patch(fs_file, filename, dir_path) -> dict:  # 接收两个字典对比
     new_fs = {}
-    permission = fs_file[0].split()[1]
+    permission = fs_file.get(list(fs_file)[0])
     for i in filename:
         if fs_file.get(i):
-            new_fs[i] = fs_file[i]
+            new_fs[re.sub(r'([^-_/a-zA-Z0-9])', r'\\\1', i)] = fs_file[i]
         else:
             if os.name == 'nt':
                 filepath = os.path.abspath(dir_path + os.sep + ".." + os.sep + i.replace('/', '\\'))
@@ -51,8 +52,7 @@ def context_patch(fs_file, filename, dir_path) -> dict:  # 接收两个字典对
                     if os.path.dirname(filepath) in e:
                         permission = e.split()[1]
                         break
-            print(f'Add [{i}:{permission}]')
-            new_fs[i] = permission
+            new_fs[re.sub(r'([^-_/a-zA-Z0-9])', r'\\\1', i)] = permission
     return new_fs
 
 
@@ -60,7 +60,7 @@ def main(dir_path, fs_config) -> None:
     origin = scan_context(os.path.abspath(fs_config))
     allfiles = scan_dir(os.path.abspath(dir_path))
     new_fs = context_patch(origin, allfiles, dir_path)
-    with open(fs_config, "w", encoding='utf-8', newline='\n') as f:
+    with open(fs_config, "w+", encoding='utf-8', newline='\n') as f:
         f.writelines([i + " " + " ".join(new_fs[i]) + "\n" for i in sorted(new_fs.keys())])
     print("Load origin %d" % (len(origin.keys())) + " entries")
     print("Detect total %d" % (len(allfiles)) + " entries")
