a8190633f624e5193aead74d1da405922e1aef8b
ColdWindScholar <3590361911@qq.com>
2023-08-15 22:29:14 +0800

    Release 2.5.5

--- win_extra.py
+++ win_extra.py
@@ -6,4 +6,4 @@ def win2wslpath(path):
     if not ":" in path:
         path = os.path.abspath(path)
     f, e = path.split(":")
-    return "".join([f"/mnt/{f.lower()}/", e.replace("\\", "/")])
+    return "".join([f"/mnt/{f.lower()}", e.replace("\\", "/")])
