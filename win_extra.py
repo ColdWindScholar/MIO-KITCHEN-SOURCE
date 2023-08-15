edce583ab1028c02dab8558579d721c26636b787
ColdWindScholar <3590361911@qq.com>
2023-08-15 22:59:08 +0800

    Release 2.5.5

--- win_extra.py
+++ win_extra.py
@@ -15,7 +15,10 @@ def wsl2winpath(path):
             path = path[1:].replace("/", "\\")
         return "\\wsl.localhost\\Ubuntu\\" + path
     else:
-        f, e = path[5:].split("/", 1)
+        if len(path) == 5:
+            f, e = path[5:], ""
+        else:
+            f, e = path[5:].split("/", 1)
         if not len(f) > 1:
             return "".join([f.upper(), ":", e.replace("/", "\\")])
         else:
