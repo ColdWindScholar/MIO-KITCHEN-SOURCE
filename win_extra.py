b7e8c11cb958857a481745e9ef41b0fb6ac25b56
ColdWindScholar <3590361911@qq.com>
2023-08-15 22:27:35 +0800

    Release 2.5.5

--- win_extra.py
+++ win_extra.py
@@ -1,1 +1,9 @@
 # Windows Extra Module For MIO-KITCHEN
+import os
+
+
+def win2wslpath(path):
+    if not ":" in path:
+        path = os.path.abspath(path)
+    f, e = path.split(":")
+    return "".join([f"/mnt/{f.lower()}/", e.replace("\\", "/")])
