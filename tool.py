5af1b488d512380405def46d9b2fee0886b5da65
ColdWindScholar <3590361911@qq.com>
2023-08-17 18:42:48 +0800

    Release 2.5.5

--- tool.py
+++ tool.py
@@ -27,7 +27,7 @@ from io import BytesIO, StringIO
 from platform import machine
 from tkinter import *
 
-if os.name == "nt1":
+if os.name == "nt":
     from tkinter import filedialog, ttk, messagebox
 else:
     from tkinter import ttk, messagebox
