17e72488da04c0b52192e2962063675a6087fbec
ColdWindScholar <3590361911@qq.com>
2023-08-17 18:40:29 +0800

    Release 2.5.5

--- mkc_filedialog.py
+++ mkc_filedialog.py
@@ -1,6 +1,6 @@
 # File Chose Extra Module For MIO-KITCHEN
 import os
-from tkinter import Toplevel, Listbox, X, Y, BOTH, MULTIPLE, LEFT, END, StringVar
+from tkinter import Toplevel, Listbox, X, BOTH, LEFT, END, StringVar
 from tkinter.ttk import *
 
 
@@ -17,7 +17,7 @@ class askopenfilename(Toplevel):
             pass
         self.type.pack(fill=X, padx=5, pady=5)
         self.path = StringVar()
-        self.paths = Entry(self,textvariable=self.path)
+        self.paths = Entry(self, textvariable=self.path)
         self.paths.setvar("/")
         self.paths.pack(fill=X, padx=5, pady=5)
         self.show = Listbox(self, activestyle='dotbox', highlightthickness=0)
@@ -62,5 +62,51 @@ class askopenfilename(Toplevel):
         return ""
 
 
-def askdirectory(title="Chose Folder"):
-    pass
+class askdirectory(Toplevel):
+    def __init__(self, title="Chose File"):
+        super().__init__()
+        self.title(title)
+        self.protocol("WM_DELETE_WINDOW", self.cancel)
+        self.path = StringVar()
+        self.paths = Entry(self, textvariable=self.path)
+        self.paths.setvar("/")
+        self.paths.pack(fill=X, padx=5, pady=5)
+        self.show = Listbox(self, activestyle='dotbox', highlightthickness=0)
+        self.show.bind("<Double-Button-1>", self.p_bind)
+        self.show.pack(fill=BOTH, padx=5, pady=5)
+        ff = Frame(self)
+        Button(ff, text="选择|Chose", command=self.return_var).pack(fill=X, side=LEFT, padx=5, pady=5)
+        Button(ff, text="刷新|Refresh", command=self.refs).pack(fill=X, side=LEFT, padx=5, pady=5)
+        Button(ff, text="取消|Cancel", command=self.cancel).pack(fill=X, side=LEFT, padx=5, pady=5)
+        ff.pack(padx=5, pady=5, fill=X)
+        self.wait_window()
+
+    def p_bind(self, event):
+        try:
+            file = self.show.get(self.show.curselection())
+        except:
+            file = ""
+        var = os.path.join(self.path.get(), file)
+        var = os.path.abspath(var)
+        if os.path.isdir(var):
+            self.path.set(var)
+            self.refs()
+        elif os.path.isfile(var):
+            self.return_var()
+
+    def refs(self):
+        self.show.delete(0, END)
+        if not self.path.get():
+            self.path.set("/")
+        for f in os.listdir(self.path.get()):
+            if os.path.isdir(os.path.join(self.path.get(),f)):
+                self.show.insert(END, f)
+
+    def return_var(self):
+        var = os.path.join(self.path.get(), self.show.get(self.show.curselection()))
+        self.destroy()
+        return var
+
+    def cancel(self):
+        self.destroy()
+        return ""
