f42e68dd929ec2ceba143b26d1e5e0997bd2db73
ColdWindScholar <3590361911@qq.com>
2023-09-23 09:05:07 +0800

    Fix Some Bugs

--- .github/aarch64/build.sh
+++ .github/aarch64/build.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/zsh
 if [ "$1" == "build" ];then
 uname -a
+python3 -m pip install -U --force-reinstall pip
+pip3 install -r requirements.txt
+sudo apt update -y && sudo apt install python3-tk -y
+pyinstaller -Fw tool.py --exclude-module=numpy -i icon.ico --collect-data sv_ttk --hidden-import=tkinter --hidden-import=PIL --hidden-import=PIL._tkinter_finder --splash splash.png
+mv dist/* ./
 exit 0
 fi
 sudo apt-get update
 sudo apt-get install docker.io
 sudo docker pull arm64v8/ubuntu
-sudo docker run arm64v8/ubuntu sudo bash ./.github/aarch64/build.sh build
+sudo docker run arm64v8/ubuntu bash ./.github/aarch64/build.sh build
