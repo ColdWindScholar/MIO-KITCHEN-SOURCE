40abefdc21a8055213ca97a2735644daefddb350
ColdWindScholar <3590361911@qq.com>
2023-09-23 08:18:14 +0800

    Fix Some Bugs

--- .github/aarch64/build.sh
+++ .github/aarch64/build.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/zsh
+clean (){
+echo "Are you sure?"
+umount
+sudo find rootfs/dev rootfs/proc rootfs/sys || true
+read 1
+sudo rm -rf rootfs
+sudo find .
+echo "bruh"
+}
 
+umount (){
+sudo umount rootfs/dev/pts rootfs/dev rootfs/proc rootfs/sys
+sudo find rootfs/dev rootfs/proc rootfs/sys
+exit 0
+}
 set -e
-
 echo "clean!!"
-./clean.sh
+clean
 echo "cleanned"
 sudo ./apk -X http://cmcc.mirrors.ustc.edu.cn/alpine/edge/main -X http://cmcc.mirrors.ustc.edu.cn/alpine/edge/community -U --allow-untrusted -p rootfs --initdb add --no-cache alpine-base coreutils bash bash-completion shadow patchelf
 
@@ -19,7 +32,7 @@ sudo mount -t proc none proc
 sudo mount -o bind /sys sys
 
 sudo env -i /usr/sbin/chroot . /usr/bin/env -i PATH=/sbin:/usr/sbin:/bin:/usr/bin TMPDIR=/tmp USER=root HOME=/root chsh -s /bin/bash
-sudo cp ../build_inside.sh tmp/build.sh
+sudo cp ./.github/aarch64/build_inside.sh tmp/build.sh
 sudo chmod +x tmp/build.sh
 sudo env -i /usr/sbin/chroot . /usr/bin/env -i PATH=/sbin:/usr/sbin:/bin:/usr/bin TMPDIR=/tmp USER=root HOME=/root bash /tmp/build.sh
 
--- .github/aarch64/umount.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/zsh
-sudo umount rootfs/dev/pts rootfs/dev rootfs/proc rootfs/sys
-sudo find rootfs/dev rootfs/proc rootfs/sys
-
-exit 0
