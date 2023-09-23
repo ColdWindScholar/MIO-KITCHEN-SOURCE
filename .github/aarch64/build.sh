6f00f60dcada2021c230cd2f49d93df3c22566cb
ColdWindScholar <3590361911@qq.com>
2023-09-23 08:48:18 +0800

    Fix Some Bugs

--- .github/aarch64/build.sh
+++ .github/aarch64/build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/zsh
+if [ "$1" == "build" ];then
+uname -r
+exit 0
+fi
 sudo apt-get update
 sudo apt-get install docker.io
 sudo docker pull arm64v8/ubuntu
-sudo docker run -it arm64v8/ubuntu /bin/bash
-uname
+sudo docker run -it arm64v8/ubuntu sudo bash ./.github/aarch64/build.sh build
