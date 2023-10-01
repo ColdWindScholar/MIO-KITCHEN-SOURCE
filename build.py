01b7320e6ed943a853dd9f6e2c4ccc8b391a0ee8
ColdWindScholar <3590361911@qq.com>
2023-10-01 18:05:47 +0800

    Fix some bugs

--- build.py
+++ build.py
@@ -1,21 +1,28 @@
 import os
 import shutil
+import platform
 
 local = os.getcwd()
 print("Building...")
-os.system("pyinstaller -F run.py --exclude-module=numpy -i icon.ico")
+if os.name == 'posix':
+    os.system(
+        "pyinstaller -Fw tool.py --exclude-module=numpy -i icon.ico --collect-data sv_ttk --hidden-import=tkinter --hidden-import=PIL --hidden-import=PIL._tkinter_finder --splash splash.png")
+elif os.name == 'nt':
+    os.system("pyinstaller -Fw tool.py --exclude-module=numpy -i icon.ico --collect-data sv_ttk --splash splash.png")
 if os.name == 'nt':
-    if os.path.exists(local + os.sep + "dist" + os.sep + "run.exe"):
-        shutil.move(local + os.sep + "dist" + os.sep + "run.exe", local)
+    if os.path.exists(local + os.sep + "dist" + os.sep + "tool.exe"):
+        shutil.move(local + os.sep + "dist" + os.sep + "tool.exe", local)
     if os.path.exists(local + os.sep + "bin" + os.sep + "Linux"):
         shutil.rmtree(local + os.sep + "bin" + os.sep + "Linux")
 elif os.name == 'posix':
-    if os.path.exists(local + os.sep + "dist" + os.sep + "run"):
-        shutil.move(local + os.sep + "dist" + os.sep + "run", local)
+    if os.path.exists(local + os.sep + "dist" + os.sep + "tool"):
+        shutil.move(local + os.sep + "dist" + os.sep + "tool", local)
+    pclist = ['images', 'languages', 'licenses', 'module', 'temp', 'extra_flash.zip', 'setting.ini',platform.system()]:
+
     if os.path.exists(local + os.sep + "bin" + os.sep + "Windows"):
         shutil.rmtree(local + os.sep + "bin" + os.sep + "Windows")
 for i in os.listdir(local):
-    if i not in ['run', 'run.exe', 'bin', 'LICENSE']:
+    if i not in ['run', 'tool.exe', 'bin', 'LICENSE']:
         print(f"Removing {i}")
         if os.path.isdir(local + os.sep + i):
             try:
@@ -32,5 +39,5 @@ for i in os.listdir(local):
 if os.name == 'posix':
     for root, dirs, files in os.walk(local, topdown=True):
         for i in files:
-            print(f"Chmod {os.path.join(root,i)}")
-            os.system(f"chmod a+x {os.path.join(root,i)}")
+            print(f"Chmod {os.path.join(root, i)}")
+            os.system(f"chmod a+x {os.path.join(root, i)}")
