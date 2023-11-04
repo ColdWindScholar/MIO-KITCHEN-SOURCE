a861c9a24aec0830b33da39beba82d2c918729cb
ColdWindScholar <3590361911@qq.com>
2023-11-04 17:52:42 +0800

    Op Some Code

--- payload_dumper.py
+++ payload_dumper.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
-import mmap
-import struct
-import hashlib
-import bz2
-import sys
 import argparse
-import bsdiff4
-import io
-import os
+import bz2
 import lzma
-import update_metadata_pb2 as um
+import os
+import struct
 import timeit
+
+import update_metadata_pb2 as um
 
 flatten = lambda l: [item for sublist in l for item in sublist]
 
@@ -26,17 +22,12 @@ def u64(x):
 def data_for_op(op, out_file, old_file):
     payloadfile.seek(data_offset + op.data_offset)
     data = payloadfile.read(op.data_length)
-
-    # assert hashlib.sha256(data).digest() == op.data_sha256_hash, 'operation data hash mismatch'
-
     if op.type == op.REPLACE_XZ:
-        dec = lzma.LZMADecompressor()
-        data = dec.decompress(data)
+        data = lzma.LZMADecompressor().decompress(data)
         out_file.seek(op.dst_extents[0].start_block * block_size)
         out_file.write(data)
     elif op.type == op.REPLACE_BZ:
-        dec = bz2.BZ2Decompressor()
-        data = dec.decompress(data)
+        data = bz2.BZ2Decompressor().decompress(data)
         out_file.seek(op.dst_extents[0].start_block * block_size)
         out_file.write(data)
     elif op.type == op.REPLACE:
@@ -45,33 +36,11 @@ def data_for_op(op, out_file, old_file):
     elif op.type == op.SOURCE_COPY:
         if not args.diff:
             print("SOURCE_COPY supported only for differential OTA")
-            sys.exit(-2)
+            exit(1)
         out_file.seek(op.dst_extents[0].start_block * block_size)
         for ext in op.src_extents:
             old_file.seek(ext.start_block * block_size)
             data = old_file.read(ext.num_blocks * block_size)
-            out_file.write(data)
-    elif op.type == op.SOURCE_BSDIFF:
-        if not args.diff:
-            print("SOURCE_BSDIFF supported only for differential OTA")
-            sys.exit(-3)
-        out_file.seek(op.dst_extents[0].start_block * block_size)
-        tmp_buff = io.BytesIO()
-        for ext in op.src_extents:
-            old_file.seek(ext.start_block * block_size)
-            old_data = old_file.read(ext.num_blocks * block_size)
-            tmp_buff.write(old_data)
-        tmp_buff.seek(0)
-        old_data = tmp_buff.read()
-        tmp_buff.seek(0)
-        tmp_buff.write(bsdiff4.patch(old_data, data))
-        n = 0
-        tmp_buff.seek(0)
-        for ext in op.dst_extents:
-            tmp_buff.seek(n * block_size)
-            n += ext.num_blocks
-            data = tmp_buff.read(ext.num_blocks * block_size)
-            out_file.seek(ext.start_block * block_size)
             out_file.write(data)
     elif op.type == op.ZERO:
         for ext in op.dst_extents:
@@ -79,7 +48,7 @@ def data_for_op(op, out_file, old_file):
             out_file.write(b'\x00' * ext.num_blocks * block_size)
     else:
         print("Unsupported type = %d\n" % op.type)
-        sys.exit(-1)
+        exit(-2)
 
     return data
 
@@ -91,24 +60,18 @@ def dump_part(part):
     else:
         print("%s:[EXTRACTING]\n" % part.partition_name)
         out_file = open('%s/%s.img' % (args.out, part.partition_name), 'wb')
-        h = hashlib.sha256()
         if args.diff:
             old_file = open('%s/%s.img' % (args.old, part.partition_name), 'rb')
         else:
             old_file = None
         for op in part.operations:
-            data = data_for_op(op, out_file, old_file)
+            data_for_op(op, out_file, old_file)
         print("%s:[%s]\n" % (part.partition_name, timeit.default_timer() - start))
 
 
 def ota_payload_dumper(payloadfile_, out='output', diff='store_true', old='old', images='', command: int = 1):
-    parser = argparse.ArgumentParser(description='OTA payload dumper')
-    parser.add_argument('--out', default=out)
-    parser.add_argument('--diff', action=diff)
-    parser.add_argument('--old', default=old)
-    parser.add_argument('--images', default=images)
     global args
-    args = parser.parse_args()
+    args = argparse.Namespace(out=out, diff=diff, old=old, images=images)
     global payloadfile
     payloadfile = payloadfile_
     args.payload = payloadfile
