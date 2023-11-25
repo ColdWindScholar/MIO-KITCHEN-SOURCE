073a3a6f6d7697698e5d8c1c2bd73768f368edcf
ColdWindScholar <3590361911@qq.com>
2023-11-25 11:44:32 +0800

    Remove Some Code

--- fspatch.py
+++ fspatch.py
@@ -115,7 +115,7 @@ def fs_patch(fs_file, dir_path) -> tuple:  # 接收两个字典对比
     return new_fs, new_add
 
 
-def main(dir_path, fs_config) -> None:
+def main(dir_path, fs_config):
     new_fs, new_add = fs_patch(scanfs(os.path.abspath(fs_config)), dir_path)
     with open(fs_config, "w", encoding='utf-8', newline='\n') as f:
         f.writelines([i + " " + " ".join(new_fs[i]) + "\n" for i in sorted(new_fs.keys())])
