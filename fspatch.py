77017480cbf8058923122775b225f9ae765a9d72
ColdWindScholar <3590361911@qq.com>
2023-11-25 11:44:56 +0800

    Remove Some Code

--- fspatch.py
+++ fspatch.py
@@ -9,7 +9,7 @@ def scanfs(file) -> dict:
         for i in file_.readlines():
             filepath, *other = i.strip().split()
             filesystem_config[filepath] = other
-            if long := len(other) > 4:
+            if (long := len(other)) > 4:
                 print(f"[Warn] {i[0]} has too much data-{long}.")
     return filesystem_config
 
