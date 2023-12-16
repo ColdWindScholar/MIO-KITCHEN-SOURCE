2db77f04e21b192738ce2e1c5b5a4d553843a968
ColdWindScholar <3590361911@qq.com>
2023-12-16 10:48:11 +0800

    Clean up code
    Signed-off-by: ColdWindScholar <3590361911@qq.com>

--- editor.py
+++ editor.py
@@ -6,7 +6,8 @@ from tkinter import ttk
 
 from utils import cz
 
-kwlist = ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del',
+kwlist = ['False', 'gettype', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue',
+          'def', 'del',
           'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda',
           'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield', "msh", 'echo', 'sed',
           'find', 'cd', 'done', 'rm', 'mkdir', "mv", "cat"]
