6b3bc023fff4b614d686c1cf399b68d3dbed1f30
ColdWindScholar <3590361911@qq.com>
2023-12-24 15:45:18 +0800

    Op Ext4 Unpack
    Signed-off-by: ColdWindScholar <3590361911@qq.com>

--- imgextractor.py
+++ imgextractor.py
@@ -27,10 +27,7 @@ class Extractor:
 
     @staticmethod
     def __out_name(file_path, out=1):
-        if out == 1:
-            name = file_path
-        else:
-            name = os.path.basename(file_path).rsplit('.', 1)[0]
+        name = file_path if out == 1 else os.path.basename(file_path).rsplit('.', 1)[0]
         name = name.split('-')[0]
         name = name.split(' ')[0]
         name = name.split('+')[0]
