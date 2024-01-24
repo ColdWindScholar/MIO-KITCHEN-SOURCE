da448b611f7fe6bb07e357f9d0b0200c276f6d6c
ColdWindScholar <3590361911@qq.com>
2024-01-24 11:37:15 +0800

    [Tool] Fix Dat Package
    Signed-off-by: ColdWindScholar <3590361911@qq.com>

--- blockimgdiff.py
+++ blockimgdiff.py
@@ -14,28 +14,33 @@
 
 from __future__ import print_function
 
-import array
-import functools
-import heapq
-import itertools
-import multiprocessing
+from array import array
+from functools import total_ordering
+from heapq import heappop, heappush, heapify
+from itertools import chain
+from multiprocessing import cpu_count
 import os
-import re
-import subprocess
-import tempfile
-import threading
+from re import sub
+from subprocess import call, STDOUT
+from tempfile import mkstemp
+from threading import Lock,Thread
 from collections import deque, OrderedDict
 from hashlib import sha1
-
 from rangelib import RangeSet
 
 __all__ = ["EmptyImage", "DataImage", "BlockImageDiff"]
 
 
+class Settings(object):
+    # Stash size cannot exceed cache_size * threshold.
+    cache_size = None
+    stash_threshold = 0.8
+
+
 def compute_patch(src, tgt, imgdiff=False):
-    srcfd, srcfile = tempfile.mkstemp(prefix="src-")
-    tgtfd, tgtfile = tempfile.mkstemp(prefix="tgt-")
-    patchfd, patchfile = tempfile.mkstemp(prefix="patch-")
+    srcfd, srcfile = mkstemp(prefix="src-")
+    tgtfd, tgtfile = mkstemp(prefix="tgt-")
+    patchfd, patchfile = mkstemp(prefix="patch-")
     os.close(patchfd)
 
     try:
@@ -51,11 +56,11 @@ def compute_patch(src, tgt, imgdiff=False):
         except OSError:
             pass
         if imgdiff:
-            p = subprocess.call(["imgdiff", "-z", srcfile, tgtfile, patchfile],
+            p = call(["imgdiff", "-z", srcfile, tgtfile, patchfile],
                                 stdout=open("/dev/null", "a"),
-                                stderr=subprocess.STDOUT)
+                                stderr=STDOUT)
         else:
-            p = subprocess.call(["bsdiff", srcfile, tgtfile, patchfile])
+            p = call(["bsdiff", srcfile, tgtfile, patchfile])
 
         if p:
             raise ValueError("diff failed: " + str(p))
@@ -206,7 +211,7 @@ class Transfer(object):
                 " to " + str(self.tgt_ranges) + ">")
 
 
-@functools.total_ordering
+@total_ordering
 class HeapItem(object):
     def __init__(self, item):
         self.item = item
@@ -267,7 +272,7 @@ class BlockImageDiff(object):
     def __init__(self, tgt, src=None, version=4, threads=None,
                  disable_imgdiff=False):
         if threads is None:
-            threads = multiprocessing.cpu_count() // 2
+            threads = cpu_count() // 2
             if threads == 0:
                 threads = 1
         self.threads = threads
@@ -324,6 +329,11 @@ class BlockImageDiff(object):
         else:
             self.ReverseBackwardEdges()
             self.ImproveVertexSequence()
+
+        # Ensure the runtime stash size is under the limit.
+        if self.version >= 2 and Settings.cache_size is not None:
+            self.ReviseStashSize()
+
         # Double-check our work.
         self.AssertSequenceGood()
 
@@ -375,7 +385,7 @@ class BlockImageDiff(object):
             for s, sr in xf.stash_before:
                 assert s not in stashes
                 if free_stash_ids:
-                    sid = heapq.heappop(free_stash_ids)
+                    sid = heappop(free_stash_ids)
                 else:
                     sid = next_stash_id
                     next_stash_id += 1
@@ -437,7 +447,7 @@ class BlockImageDiff(object):
                             free_size += sr.size()
                             free_string.append("free %s\n" % sh)
                             stashes.pop(sh)
-                    heapq.heappush(free_stash_ids, sid)
+                    heappush(free_stash_ids, sid)
 
                 if unstashed_src_ranges.size() > 0:
                     src_str.insert(1, unstashed_src_ranges.to_string_raw())
@@ -545,6 +555,21 @@ class BlockImageDiff(object):
                 out.append("".join(free_string))
                 stashed_blocks -= free_size
 
+            if self.version >= 2 and Settings.cache_size is not None:
+                # Sanity check: abort if we're going to need more stash space than
+                # the allowed size (cache_size * threshold). There are two purposes
+                # of having a threshold here. a) Part of the cache may have been
+                # occupied by some recovery logs. b) It will buy us some time to deal
+                # with the oversize issue.
+                cache_size = Settings.cache_size
+                stash_threshold = Settings.stash_threshold
+                max_allowed = cache_size * stash_threshold
+                assert max_stashed_blocks * self.tgt.blocksize < max_allowed, \
+                    'Stash size %d (%d * %d) exceeds the limit %d (%d * %.2f)' % (
+                        max_stashed_blocks * self.tgt.blocksize, max_stashed_blocks,
+                        self.tgt.blocksize, max_allowed, cache_size,
+                        stash_threshold)
+
         if self.version >= 3:
             self.touched_src_sha1 = self.HashBlocks(
                 self.src, self.touched_src_ranges)
@@ -587,7 +612,15 @@ class BlockImageDiff(object):
 
         if self.version >= 2:
             self._max_stashed_size = max_stashed_blocks * self.tgt.blocksize
-            print("max stashed blocks: %d  (%d bytes), limit: <unknown>\n" % (
+            OPTIONS = Settings
+            if OPTIONS.cache_size is not None:
+                max_allowed = OPTIONS.cache_size * OPTIONS.stash_threshold
+                print("max stashed blocks: %d  (%d bytes), "
+                      "limit: %d bytes (%.2f%%)\n" % (
+                          max_stashed_blocks, self._max_stashed_size, max_allowed,
+                          self._max_stashed_size * 100.0 / max_allowed))
+            else:
+                print("max stashed blocks: %d  (%d bytes), limit: <unknown>\n" % (
                     max_stashed_blocks, self._max_stashed_size))
 
     def ReviseStashSize(self):
@@ -607,8 +640,8 @@ class BlockImageDiff(object):
 
         # Compute the maximum blocks available for stash based on /cache size and
         # the threshold.
-        cache_size = None
-        stash_threshold = 0.8
+        cache_size = Settings.cache_size
+        stash_threshold = Settings.stash_threshold
         max_allowed = cache_size * stash_threshold / self.tgt.blocksize
 
         stashed_blocks = 0
@@ -736,7 +769,7 @@ class BlockImageDiff(object):
             patches = [None] * patch_num
 
             # TODO: Rewrite with multiprocessing.ThreadPool?
-            lock = threading.Lock()
+            lock = Lock()
 
             def diff_worker():
                 while True:
@@ -753,7 +786,7 @@ class BlockImageDiff(object):
                             xf.tgt_name if xf.tgt_name == xf.src_name else (
                                     xf.tgt_name + " (from " + xf.src_name + ")")))
 
-            threads = [threading.Thread(target=diff_worker)
+            threads = [Thread(target=diff_worker)
                        for _ in range(self.threads)]
             for th in threads:
                 th.start()
@@ -776,7 +809,7 @@ class BlockImageDiff(object):
         # - we write every block we care about exactly once.
 
         # Start with no blocks having been touched yet.
-        touched = array.array("B", (0,) * self.tgt.total_blocks)
+        touched = array("B", (0,) * self.tgt.total_blocks)
 
         # Imagine processing the transfers in order.
         for xf in self.transfers:
@@ -831,15 +864,15 @@ class BlockImageDiff(object):
         # executed.
         S = [(u.NetStashChange(), u.order, u) for u in self.transfers
              if not u.incoming]
-        heapq.heapify(S)
+        heapify(S)
 
         while S:
-            _, _, xf = heapq.heappop(S)
+            _, _, xf = heappop(S)
             L.append(xf)
             for u in xf.outgoing:
                 del u.incoming[xf]
                 if not u.incoming:
-                    heapq.heappush(S, (u.NetStashChange(), u.order, u))
+                    heappush(S, (u.NetStashChange(), u.order, u))
 
         # if this fails then our graph had a cycle.
         assert len(L) == len(self.transfers)
@@ -855,6 +888,7 @@ class BlockImageDiff(object):
         lost_source = 0
 
         for xf in self.transfers:
+            lost = 0
             size = xf.src_ranges.size()
             for u in xf.goes_before:
                 # xf should go before u
@@ -956,7 +990,7 @@ class BlockImageDiff(object):
         for xf in self.transfers:
             xf.heap_item = HeapItem(xf)
             heap.append(xf.heap_item)
-        heapq.heapify(heap)
+        heapify(heap)
 
         sinks = set(u for u in G if not u.outgoing)
         sources = set(u for u in G if not u.incoming)
@@ -965,7 +999,7 @@ class BlockImageDiff(object):
             iu.score += delta
             iu.heap_item.clear()
             iu.heap_item = HeapItem(iu)
-            heapq.heappush(heap, iu.heap_item)
+            heappush(heap, iu.heap_item)
 
         while G:
             # Put all sinks at the end of the sequence.
@@ -999,7 +1033,7 @@ class BlockImageDiff(object):
             # pretending it's a source rather than a sink.
 
             while True:
-                u = heapq.heappop(heap)
+                u = heappop(heap)
                 if u and u.item in G:
                     u = u.item
                     break
@@ -1018,7 +1052,7 @@ class BlockImageDiff(object):
         # and by rearranging self.transfers to be in the chosen sequence.
 
         new_transfers = []
-        for x in itertools.chain(s1, s2):
+        for x in chain(s1, s2):
             x.order = len(new_transfers)
             new_transfers.append(x)
             del x.incoming
@@ -1043,7 +1077,7 @@ class BlockImageDiff(object):
                         source_ranges[i] = b
                     else:
                         if not isinstance(source_ranges[i], set):
-                            source_ranges[i] = set([source_ranges[i]])
+                            source_ranges[i] = {source_ranges[i]}
                         source_ranges[i].add(b)
 
         for a in self.transfers:
@@ -1059,7 +1093,8 @@ class BlockImageDiff(object):
                             intersections.add(b)
 
             for b in intersections:
-                if a is b: continue
+                if a is b:
+                    continue
 
                 # If the blocks written by A are read by B, then B needs to go before A.
                 i = a.tgt_ranges.intersect(b.src_ranges)
@@ -1098,7 +1133,7 @@ class BlockImageDiff(object):
                 return
 
             pieces = 0
-            cache_size = None
+            cache_size = Settings.cache_size
             split_threshold = 0.125
             max_blocks_per_transfer = int(cache_size * split_threshold /
                                           self.tgt.blocksize)
@@ -1163,7 +1198,7 @@ class BlockImageDiff(object):
                             "diff", self.transfers, self.version >= 3)
                 continue
 
-            b = re.sub("[0-9]+", "#", b)
+            b = sub("[0-9]+", "#", b)
             if b in self.src_numpatterns:
                 # Look for a 'number pattern' match (a basename match after
                 # all runs of digits are replaced by "#").  (This is useful
@@ -1180,7 +1215,7 @@ class BlockImageDiff(object):
         for k in self.src.file_map.keys():
             b = os.path.basename(k)
             self.src_basenames[b] = k
-            b = re.sub("[0-9]+", "#", b)
+            b = sub("[0-9]+", "#", b)
             self.src_numpatterns[b] = k
 
     @staticmethod
