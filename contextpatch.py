dc88ea13b159e313e3635862444c87332d7121ec
ColdWindScholar <3590361911@qq.com>
2024-02-13 18:53:09 +0800

    Add Context Process
    Signed-off-by: ColdWindScholar <3590361911@qq.com>

--- contextpatch.py
+++ contextpatch.py
@@ -23,7 +23,8 @@ def scan_context(file) -> dict:  # 读取context文件返回一个字典
             filepath, *other = i.strip().split()
             context[filepath] = other
             if len(other) > 1:
-                print(f"[Warn] {i[0]} has too much data.")
+                print(f"[Warn] {i[0]} has too much data.Automatic processing.")
+                context[filepath] = other.replace(" ", '')
     return context
 
 
