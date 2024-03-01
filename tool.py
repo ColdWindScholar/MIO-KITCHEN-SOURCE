3b8e2dc5f666c868bf4452972bce6565d0df7734
ColdWindScholar <3590361911@qq.com>
2024-03-01 19:34:43 +0800

    - Fix Some Bugs
    Signed-off-by: ColdWindScholar <3590361911@qq.com>

--- tool.py
+++ tool.py
@@ -38,11 +38,10 @@ from tkinter import ttk, messagebox
 from shutil import rmtree, copy, move
 import requests
 import sv_ttk
-
-if sys.version_info.major == 3 and sys.version_info.minor > 12:
-    print(f"Not Support [{sys.version}] yet\nSorry for any inconvenience caused")
-    input()
-    sys.exit(1)
+if sys.version_info.major == 3:
+    if sys.version_info.minor < 8 or sys.version_info.minor > 12:
+        input(f"Not Support [{sys.version}] yet\nSorry for any inconvenience caused")
+        sys.exit(1)
 from PIL import Image, ImageTk
 import fspatch
 import imgextractor
