4ff06926194b94ccc50b39daa8785ede7726ea58
ColdWindScholar <3590361911@qq.com>
2024-04-27 01:05:05 +0800

    Update Some Code
    Signed-off-by: ColdWindScholar <3590361911@qq.com>

--- mkc_filedialog.py
+++ mkc_filedialog.py
@@ -56,7 +56,7 @@ class askopenfilenames(Toplevel):
     def refs(self):
         self.show.delete(0, END)
         if not self.path.get():
-            self.path.set("/")
+            self.path.set(os.path.abspath("/"))
         f, e = self.type.get().replace("*", "").split(".")
         for f in os.listdir(self.path.get()):
             if f.startswith(f) and f.endswith(e):
@@ -86,7 +86,7 @@ class askdirectorys(Toplevel):
         self.path = StringVar()
         self.paths = Entry(self, textvariable=self.path)
         self.paths.bind("<Return>", self.p_bind)
-        self.path.set("/")
+        self.path.set(os.path.abspath("/"))
         self.paths.pack(fill=X, padx=5, pady=5)
         self.show = Listbox(self, activestyle='dotbox', highlightthickness=0)
         self.show.bind("<Double-Button-1>", self.p_bind)
