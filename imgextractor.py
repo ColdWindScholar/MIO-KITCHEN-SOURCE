adbbb7b75748cb1ec359d47e843b280cbfb4ad28
ColdWindScholar <3590361911@qq.com>
2024-05-05 10:47:18 +0800

    - Update Some Code
    Signed-off-by: ColdWindScholar <3590361911@qq.com>

--- imgextractor.py
+++ imgextractor.py
@@ -85,108 +85,110 @@ class Extractor:
                     w += 1
         return f'{s}{o}{g}{w}'
 
-    def __ext4extractor(self):
-        def scan_dir(root_inode, root_path=""):
-            for entry_name, entry_inode_idx, entry_type in root_inode.open_dir():
-                if entry_name in ['.', '..'] or entry_name.endswith(' (2)'):
-                    continue
-                entry_inode = root_inode.volume.get_inode(entry_inode_idx, entry_type)
-                entry_inode_path = root_path + '/' + entry_name
-                if entry_inode_path[-1:] == '/' and not entry_inode.is_dir:
-                    continue
+    def scan_dir(self, root_inode, root_path=""):
+        for entry_name, entry_inode_idx, entry_type in root_inode.open_dir():
+            if entry_name in ['.', '..'] or entry_name.endswith(' (2)'):
+                continue
+            entry_inode = root_inode.volume.get_inode(entry_inode_idx, entry_type)
+            entry_inode_path = root_path + '/' + entry_name
+            if entry_inode_path[-1:] == '/' and not entry_inode.is_dir:
+                continue
 
-                mode = self.__get_perm(entry_inode.mode_str)
-                uid = entry_inode.inode.i_uid
-                gid = entry_inode.inode.i_gid
-                cap = ''
-                link_target = ''
-                tmp_path = self.FileName + entry_inode_path
-                for f, e in entry_inode.xattrs():
-                    if f == 'security.selinux':
-                        for fuk_ in '\\^$.|?*+(){}[]':
-                            t_p_mkc = tmp_path.replace(fuk_, f'\\{fuk_}')
-                        self.context.append(f"/{t_p_mkc} {e.decode('utf8')[:-1]}")
-                    elif f == 'security.capability':
-                        r = struct.unpack('<5I', e)
-                        if r[1] > 65535:
-                            cap = hex(int(f'{r[3]:04x}{r[1]:04x}', 16))
-                        else:
-                            cap = hex(int(f'{r[3]:04x}{r[2]:04x}{r[1]:04x}', 16))
-                        cap = f" capabilities={cap}"
-                if entry_inode.is_symlink:
-                    try:
-                        link_target = entry_inode.open_read().read().decode("utf8")
-                    except Exception and BaseException:
-                        link_target_block = int.from_bytes(entry_inode.open_read().read(), "little")
-                        link_target = root_inode.volume.read(link_target_block * root_inode.volume.block_size,
-                                                             entry_inode.inode.i_size).decode("utf8")
-                if tmp_path.find(' ', 1, len(tmp_path)) > 0:
-                    self.space.append(tmp_path)
-                    self.fs_config.append(
-                        f"{tmp_path.replace(' ', '_')} {uid} {gid} {mode}{cap} {link_target}")
-                else:
-                    self.fs_config.append(
-                        f'{tmp_path} {uid} {gid} {mode}{cap} {link_target}')
-                if entry_inode.is_dir:
-                    dir_target = self.EXTRACT_DIR + entry_inode_path.replace(' ', '_').replace('"', '')
-                    if dir_target.endswith('.') and os.name == 'nt':
-                        dir_target = dir_target[:-1]
-                    if not os.path.isdir(dir_target):
-                        os.makedirs(dir_target)
-                    if os.name == 'posix' and os.geteuid() == 0:
-                        os.chmod(dir_target, int(mode, 8))
-                        os.chown(dir_target, uid, gid)
-                    scan_dir(entry_inode, entry_inode_path)
-                elif entry_inode.is_file:
-                    file_target = self.EXTRACT_DIR + entry_inode_path.replace(' ', '_').replace('"', '')
-                    try:
-                        with open(file_target, 'wb') as out:
-                            out.write(entry_inode.open_read().read())
-                    except Exception and BaseException as e:
-                        print(f'[E] Cannot Write to {file_target}, Reason: {e}')
-                    if os.name == 'posix' and os.geteuid() == 0:
-                        os.chmod(file_target, int(mode, 8))
-                        os.chown(file_target, uid, gid)
-                elif entry_inode.is_symlink:
-                    target = self.EXTRACT_DIR + entry_inode_path.replace(' ', '_')
-                    try:
-                        if os.path.islink(target) or os.path.isfile(target):
+            mode = self.__get_perm(entry_inode.mode_str)
+            uid = entry_inode.inode.i_uid
+            gid = entry_inode.inode.i_gid
+            cap = ''
+            link_target = ''
+            tmp_path = self.FileName + entry_inode_path
+            for f, e in entry_inode.xattrs():
+                if f == 'security.selinux':
+                    t_p_mkc = tmp_path
+                    for fuk_ in '\\^$.|?*+(){}[]':
+                        t_p_mkc = t_p_mkc.replace(fuk_, f'\\{fuk_}')
+                    self.context.append(f"/{t_p_mkc} {e.decode('utf8')[:-1]}")
+                elif f == 'security.capability':
+                    r = struct.unpack('<5I', e)
+                    if r[1] > 65535:
+                        cap = hex(int(f'{r[3]:04x}{r[1]:04x}', 16))
+                    else:
+                        cap = hex(int(f'{r[3]:04x}{r[2]:04x}{r[1]:04x}', 16))
+                    cap = f" capabilities={cap}"
+            if entry_inode.is_symlink:
+                try:
+                    link_target = entry_inode.open_read().read().decode("utf8")
+                except Exception and BaseException:
+                    link_target_block = int.from_bytes(entry_inode.open_read().read(), "little")
+                    link_target = root_inode.volume.read(link_target_block * root_inode.volume.block_size,
+                                                         entry_inode.inode.i_size).decode("utf8")
+            if tmp_path.find(' ', 1, len(tmp_path)) > 0:
+                self.space.append(tmp_path)
+                self.fs_config.append(
+                    f"{tmp_path.replace(' ', '_')} {uid} {gid} {mode}{cap} {link_target}")
+            else:
+                self.fs_config.append(
+                    f'{tmp_path} {uid} {gid} {mode}{cap} {link_target}')
+            if entry_inode.is_dir:
+                dir_target = self.EXTRACT_DIR + entry_inode_path.replace(' ', '_').replace('"', '')
+                if dir_target.endswith('.') and os.name == 'nt':
+                    dir_target = dir_target[:-1]
+                if not os.path.isdir(dir_target):
+                    os.makedirs(dir_target)
+                if os.name == 'posix' and os.geteuid() == 0:
+                    os.chmod(dir_target, int(mode, 8))
+                    os.chown(dir_target, uid, gid)
+                self.scan_dir(entry_inode, entry_inode_path)
+            elif entry_inode.is_file:
+                file_target = self.EXTRACT_DIR + entry_inode_path.replace(' ', '_').replace('"', '')
+                try:
+                    with open(file_target, 'wb') as out:
+                        out.write(entry_inode.open_read().read())
+                except Exception and BaseException as e:
+                    print(f'[E] Cannot Write to {file_target}, Reason: {e}')
+                if os.name == 'posix' and os.geteuid() == 0:
+                    os.chmod(file_target, int(mode, 8))
+                    os.chown(file_target, uid, gid)
+            elif entry_inode.is_symlink:
+                target = self.EXTRACT_DIR + entry_inode_path.replace(' ', '_')
+                try:
+                    if os.path.islink(target) or os.path.isfile(target):
+                        try:
+                            os.remove(target)
+                        finally:
+                            ...
+                    if os.name == 'posix':
+                        os.symlink(link_target, target)
+                    elif os.name == 'nt':
+                        with open(target.replace('/', os.sep), 'wb') as out:
+                            out.write(b'!<symlink>' + link_target.encode('utf-16') + b'\x00\x00')
                             try:
-                                os.remove(target)
-                            finally:
-                                ...
-                        if os.name == 'posix':
-                            os.symlink(link_target, target)
-                        elif os.name == 'nt':
-                            with open(target.replace('/', os.sep), 'wb') as out:
-                                out.write(b'!<symlink>' + link_target.encode('utf-16') + b'\x00\x00')
+                                windll.kernel32.SetFileAttributesA(LPCSTR(target.encode()),
+                                                                   DWORD(FILE_ATTRIBUTE_SYSTEM))
+                            except Exception as e:
+                                print(e.__str__())
+                except BaseException and Exception:
+                    try:
+                        if link_target and link_target.isprintable():
+                            if os.name == 'posix':
+                                os.symlink(link_target, target)
+                            elif os.name == 'nt':
+                                with open(target.replace('/', os.sep), 'wb') as out:
+                                    out.write(b'!<symlink>' + link_target.encode('utf-16') + b'\x00\x00')
                                 try:
                                     windll.kernel32.SetFileAttributesA(LPCSTR(target.encode()),
                                                                        DWORD(FILE_ATTRIBUTE_SYSTEM))
                                 except Exception as e:
                                     print(e.__str__())
-                    except BaseException and Exception:
-                        try:
-                            if link_target and link_target.isprintable():
-                                if os.name == 'posix':
-                                    os.symlink(link_target, target)
-                                elif os.name == 'nt':
-                                    with open(target.replace('/', os.sep), 'wb') as out:
-                                        out.write(b'!<symlink>' + link_target.encode('utf-16') + b'\x00\x00')
-                                    try:
-                                        windll.kernel32.SetFileAttributesA(LPCSTR(target.encode()),
-                                                                           DWORD(FILE_ATTRIBUTE_SYSTEM))
-                                    except Exception as e:
-                                        print(e.__str__())
-                        finally:
-                            ...
+                    finally:
+                        ...
+
+    def __ext4extractor(self):
 
         if not os.path.isdir(self.CONFIG_DIR):
             os.makedirs(self.CONFIG_DIR)
         self.__write(os.path.getsize(self.OUTPUT_IMAGE_FILE), self.CONFIG_DIR + os.sep + self.FileName + '_size.txt')
         with open(self.OUTPUT_IMAGE_FILE, 'rb') as file:
             dir_r = self.FileName
-            scan_dir(ext4.Volume(file).root)
+            self.scan_dir(ext4.Volume(file).root)
             self.fs_config.insert(0, '/ 0 2000 0755' if dir_r == 'vendor' else '/ 0 0 0755')
             self.fs_config.insert(1, f'{dir_r} 0 2000 0755' if dir_r == 'vendor' else '/lost+found 0 0 0700')
             self.fs_config.insert(2 if dir_r == 'system' else 1, f'{dir_r} 0 0 0755')
