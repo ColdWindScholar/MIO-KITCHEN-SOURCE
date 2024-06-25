7bf20dbb36a618145d421db36eb44a339824e951
ColdWindScholar <3590361911@qq.com>
2024-06-25 19:37:54 +0800

    - Update Some Code
    Signed-off-by: ColdWindScholar <3590361911@qq.com>

--- config_parser.py
+++ config_parser.py
@@ -20,6 +20,22 @@ class ConfigParser:
                     else:
                         continue
 
+    def read_string(self, s):
+        for i in s.splitlines():
+            i = i.strip()
+            if not i or i.startswith("#"):
+                continue
+            if i.startswith('[') and i.endswith(']'):
+                self.dict_index = i[1:-1]
+                self.dict[self.dict_index] = {}
+            else:
+                if '=' in i:
+                    n, *v = i.split("=")
+                    n, v = n.strip(), v[0].strip()
+                    self.dict[self.dict_index][n] = v
+                else:
+                    continue
+
     def items(self, item):
         items = self.dict[item]
         if item not in self.dict.keys():
@@ -38,3 +54,8 @@ class ConfigParser:
         if main_i not in self.dict.keys():
             self.dict[main_i] = {}
         self.dict[main_i][n] = v
+
+    def get(self, main_i, n):
+        if main_i not in self.dict.keys():
+            return ''
+        return self.dict[main_i][n]
