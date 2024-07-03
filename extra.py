19fa7b812c425b092beb557ed5599f7e91311d08
ColdWindScholar <3590361911@qq.com>
2024-07-03 23:41:33 +0800

    - Update Some Code
    Signed-off-by: ColdWindScholar <3590361911@qq.com>

--- extra.py
+++ extra.py
@@ -4,33 +4,6 @@ import re
 import contextpatch
 import fspatch
 from posix import symlink
-import zstandard
-
-
-class ZstdImageExtract:
-    def __init__(self, f, o):
-        self.decoder = zstandard.ZstdDecompressor()
-        self.file = f
-        self.output = o
-        self.BUFSIZE = 8192
-        self.ALIGN = 0x2000000
-
-    def extract(self, overwrite=False):
-        file_size = os.path.getsize(self.file)
-        if file_size < self.ALIGN:
-            cnt = 1
-        else:
-            cnt = file_size // self.ALIGN + 1
-        with open(self.file, 'rb') as f, open(self.output, 'wb') as f2:
-            for i in range(cnt):
-                f.seek(i * self.ALIGN)
-                dec = self.decoder.decompressobj()
-                while not dec.eof:
-                    f2.write(dec.decompress(f.read(self.BUFSIZE)))
-                f2.write(dec.flush())
-        if overwrite:
-            os.remove(self.file)
-            os.rename(self.output, self.file)
 
 
 class updaterutil:
