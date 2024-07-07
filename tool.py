b42340fa1d217b3612240ece2efe55fd24dfe8ea
ColdWindScholar <3590361911@qq.com>
2024-07-07 13:36:24 +0800

    - Update Some Code
    Signed-off-by: ColdWindScholar <3590361911@qq.com>

--- tool.py
+++ tool.py
@@ -1875,7 +1875,7 @@ class MpkStore(Toplevel):
             f2 = ttk.Frame(fb)
             ttk.Label(f, image=PhotoImage(data=images.none_byte)).pack(side=LEFT, padx=5, pady=5)
             #ttk.Label(f2, text=f"{data.get('name')[:6]}").pack(side=LEFT, padx=5, pady=5)
-            a = ttk.Label(f2, text=f"{lang.t21}:{data.get('author')} {lang.t22}:{data.get('version')} Size:{self.hum_convert(data.get('size'))}"[:40])
+            a = ttk.Label(f2, text=f"{lang.t21}{data.get('author')} {lang.t22}{data.get('version')} Size:{self.hum_convert(data.get('size'))}"[:42])
             a.pack_propagate(False)
             a.pack(side=LEFT, padx=5, pady=5)
             f2.pack(side=TOP)
