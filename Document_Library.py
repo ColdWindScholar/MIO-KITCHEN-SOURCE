ec5d16914f5bdb4aec86808de5180c0e852c2655
ColdWindScholar <3590361911@qq.com>
2024-07-11 10:55:20 +0800

    - Update Some Code
    Signed-off-by: ColdWindScholar <3590361911@qq.com>

--- Document_Library.py
+++ Document_Library.py
@@ -1,4 +1,17 @@
 """AI Document For MIO-KITCHEN"""
+# Copyright (C) 2024 The MIO-KITCHEN-SOURCE Project
+#
+# Licensed under the GNU AFFERO GENERAL PUBLIC LICENSE, Version 3.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.gnu.org/licenses/agpl-3.0.en.html#license-text
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 library = {
     "error: ext4_allocate_best_fit_partial: failed to allocate xxx blocks, out of space?": {
         "en": "You should increase the size for the img you are packing, if u use <auto>, You should switch to the <original size> , and modify <PARTNAME>_size.txt(under <config> folder) to increase the size of the image, and try again",
--- contextpatch.py
+++ contextpatch.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # pylint: disable=line-too-long
+# Copyright (C) 2024 The MIO-KITCHEN-SOURCE Project
+#
+# Licensed under the GNU AFFERO GENERAL PUBLIC LICENSE, Version 3.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.gnu.org/licenses/agpl-3.0.en.html#license-text
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import os
 from re import escape
 
