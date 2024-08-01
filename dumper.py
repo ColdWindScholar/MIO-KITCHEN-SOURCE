66e59c98759f736b5fc252b453afaf8915b54908
小さい猫 <chiisaineko@protonmail.com>
2024-08-02 01:15:45 +0900

    Correct https://github.com/ColdWindScholar/MIO-KITCHEN-SOURCE/commit/1ad5c5f60067a19858fe0f6d8675c4cddbbbf026

--- dumper.py
+++ dumper.py
@@ -123,10 +123,9 @@ class Dumper:
         op = operation["operation"]
 
         # assert hashlib.sha256(data).digest() == op.data_sha256_hash, 'operation data hash mismatch'
-        if op.type == op.REPLACE_ZSTD and payloadfile.read(4) == b'\x28\xb5\x2f\xfd':
-            op_type = op.REPLACE_ZSTD
-        else:
-            op_type = op.type
+        op_type = op.type
+        if op.type == op.REPLACE_ZSTD and payloadfile.read(4) != b'\x28\xb5\x2f\xfd':
+            op_type = op.REPLACE
         payloadfile.seek(payloadfile.tell() - 4)
         if op_type == op.REPLACE_ZSTD:
             dec = zstandard.ZstdDecompressor().decompressobj()
