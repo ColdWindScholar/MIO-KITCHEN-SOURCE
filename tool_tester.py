1adc1d77493fbf18145bb52ccd4964f2def11a7c
ColdWindScholar <3590361911@qq.com>
2024-08-06 17:23:46 +0800

    - [Readme] Update Info
    Signed-off-by: ColdWindScholar <3590361911@qq.com>

--- tool_tester.py
+++ tool_tester.py
@@ -1,3 +1,4 @@
+import platform
 import unittest
 import os
 import sys
@@ -6,6 +7,8 @@ if os.name == 'nt':
     prog_path = os.getcwd()
 else:
     prog_path = os.path.normpath(os.path.abspath(os.path.dirname(sys.argv[0])))
+
+tool_bin = os.path.join(prog_path, 'bin', platform.system(), platform.machine()) + os.sep
 
 
 class Test(unittest.TestCase):
@@ -22,6 +25,21 @@ class Test(unittest.TestCase):
         for i in pys:
             print(f'Importing {i}')
             __import__(i)
+
+    unittest.skipIf(platform.system() == 'Darwin', "Skip Macos")
+
+    def test_binaries(self):
+        file_list = ['brotli', 'busybox', 'cpio', 'dtc', 'e2fsdroid', 'extract.erofs', 'extract.f2fs', 'img2simg',
+                     'lpmake', 'magiskboot', 'make_ext4fs', 'mke2fs', 'mkfs.erofs', 'mkfs.f2fs', 'sload.f2fs', 'zstd']
+        if os.name == 'nt':
+            file_list.remove('mkfs.f2fs')
+            file_list.remove('extract.f2fs')
+            file_list = [i + '.exe' for i in file_list]
+            file_list.append('cygwin1.dll')
+            file_list.append('mv.exe')
+        for i in file_list:
+            if not os.path.exists(os.path.join(tool_bin, i)):
+                raise FileNotFoundError(f'{i} is missing!')
 
     def tearDown(self):
         print('Test Done!')
