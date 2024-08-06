190f3bd3a91e67a9e5baddda0e085cc0aa5ab0ba
ColdWindScholar <3590361911@qq.com>
2024-08-06 17:00:50 +0800

    - [Readme] Update Info
    Signed-off-by: ColdWindScholar <3590361911@qq.com>

---
+++ tool_tester.py
@@ -0,0 +1,32 @@
+import unittest
+import os
+import sys
+
+if os.name == 'nt':
+    prog_path = os.getcwd()
+else:
+    prog_path = os.path.normpath(os.path.abspath(os.path.dirname(sys.argv[0])))
+
+from tool import *
+
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        print(
+            'Starting Test.'
+        )
+
+    def test_import(self):
+        pys = [i[:-3] for i in os.listdir(prog_path) if i.endswith('.py') and i != 'build.py']
+        pys.append('tkinter')
+        sys.path.append(prog_path)
+        for i in pys:
+            print(f'Importing {i}')
+            __import__(i)
+
+    def tearDown(self):
+        print('Test Done!')
+
+
+if __name__ == '__main__':
+    unittest.main()
