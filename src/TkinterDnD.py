c378d0fde0440fd808a3bff52051e8b0f08ccd52
ColdWindScholar <3590361911@qq.com>
2024-12-08 00:58:00 +0800

    - [tool] update codes
    Signed-off-by: ColdWindScholar <3590361911@qq.com>

--- src/TkinterDnD.py
+++ src/TkinterDnD.py
@@ -33,8 +33,7 @@ def _require(tkroot):
         import os.path
         import platform
         mach_ = platform.machine()
-        if platform.architecture()[0] == '32bit' and mach_ == 'AMD64': mach_ = 'x86'
-        platform.machine = lambda: mach_
+        platform.machine = lambda: 'x86' if platform.architecture()[0] == '32bit' and mach_ == 'AMD64' else mach_
         if platform.system()=="Darwin" and platform.machine()=="arm64":
             tkdnd_platform_rep = "osx-arm64"
         elif platform.system()=="Darwin" and platform.machine()=="x86_64":
