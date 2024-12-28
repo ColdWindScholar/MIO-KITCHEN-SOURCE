d471fa6618b6a08da38a3a3e80f52244dce349c3
ColdWindScholar <3590361911@qq.com>
2024-12-28 13:26:07 +0800

    - [tool] update coeds
    Signed-off-by: ColdWindScholar <3590361911@qq.com>

--- src/qtui/__init__.py
+++ src/qtui/__init__.py
@@ -1,3 +1,5 @@
+from PyQt6.QtGui import QIcon, QPixmap
+from ..core.images import icon_byte
 from PyQt6.QtWidgets import QMainWindow, QWidget
 class Error(QWidget):
     def __init__(self, code, desc="unknown error"):
@@ -8,3 +10,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("MIO-KITCHEN")
+        pm = QPixmap()
+        pm.loadFromData(icon_byte, "png")
+        windows_icon = QIcon(pm)
+        self.setWindowIcon(windows_icon)
