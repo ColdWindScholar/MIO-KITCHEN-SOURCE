e7aac6914c7cd28460f09d7f025c350238ae4ed5
ColdWindScholar <3590361911@qq.com>
2025-02-23 13:51:21 +0800

    - [tool] Update Codes.
    Signed-off-by: ColdWindScholar <3590361911@qq.com>

--- src/core/cpio.py
+++ src/core/cpio.py
@@ -1,3 +1,17 @@
+# pylint: disable=line-too-long, missing-class-docstring, missing-function-docstring
+# Copyright (C) 2022-2025 The MIO-KITCHEN-SOURCE Project
+#
+# Licensed under the GNU AFFERO GENERAL PUBLIC LICENSE, Version 3.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.gnu.org/licenses/agpl-3.0.en.html#license-text
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import os.path
 from ctypes import sizeof, c_char, LittleEndianStructure, byref, memmove, string_at
 from enum import Enum
