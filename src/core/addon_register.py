9d2bfeb2cf887153e69f78b3fa1504d3e5c6b8eb
ColdWindScholar <3590361911@qq.com>
2025-03-08 12:42:19 +0800

    - [tool] Update Codes.
    Signed-off-by: ColdWindScholar <3590361911@qq.com>

--- src/core/addon_register.py
+++ src/core/addon_register.py
@@ -1,9 +1,10 @@
 import logging
-try:
-    from enum import IntEnum
-except ImportError:
-    IntEnum = int
-class Entry(IntEnum):
+
+from enum import Enum
+
+
+
+class Entry(Enum):
     # Normal Entry
     main = 0
     # When  Pack Rom
@@ -14,16 +15,20 @@ class Entry(IntEnum):
     close = 2
     # When the tool boot
     boot = 3
-class Type(IntEnum):
+
+
+class Type(Enum):
     normal = 0
     environment = 1
+
 
 class PluginLoader(object):
     def __init__(self):
         self.plugins = {}
         self.virtual = {}
 
-    def register(self, id_:str="addon", entry:Entry=Entry, func:None=None, virtual:bool = False, virtual_info:dict=None):
+    def register(self, id_: str = "addon", entry: Entry = Entry, func: None = None, virtual: bool = False,
+                 virtual_info: dict = None):
         if not func:
             logging.debug(f"{entry} of {id_} is {func}!")
         if id_ not in self.plugins:
@@ -31,33 +36,32 @@ class PluginLoader(object):
         self.plugins[id_][entry] = func
         if virtual and not virtual_info:
             virtual_info = {
-                    "id":id_,
-                    "name": id_,
-                    "author":"",
-                    "version":"",
-                }
+                "id": id_,
+                "name": id_,
+                "author": "",
+                "version": "",
+            }
             self.virtual[id_] = virtual_info
         if entry == Entry.boot:
             self.run(id_, entry)
 
-    def run(self, id_:str="addon", entry:Entry=Entry, *args, **kwargs):
+    def run(self, id_: str = "addon", entry: Entry = Entry, *args, **kwargs):
         if not id_ in self.plugins.keys():
-            logging.info(f"{id_} not in list")
+            print(f"{id_} is not callable.")
             return
         if not entry in self.plugins[id_].keys():
-            logging.info(f"{entry} not in {id_}")
+            print(f"{entry} not in {id_}")
             return
         try:
             return self.plugins[id_][entry](*args, **kwargs)
         except TypeError:
             return self.plugins[id_][entry]()
 
-    def run_entry(self, entry:Entry):
+    def run_entry(self, entry: Entry):
         for id_ in self.plugins:
             if not entry in self.plugins[id_].keys():
                 continue
             self.run(id_, entry)
-
 
 
 loader = PluginLoader()
