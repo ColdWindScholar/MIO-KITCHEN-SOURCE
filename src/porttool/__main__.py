f8444556600ab287c21624e413a1f4a3c2a75937
ColdWindScholar <3590361911@qq.com>
2025-08-13 04:17:19 +0800

    - [Tool] update codes

--- src/porttool/__main__.py
+++ src/porttool/__main__.py
@@ -14,6 +14,7 @@ class Main(Toplevel):
     def __init__(self):
         super().__init__()
         self.title("MTK Port Tool")
+        self.resizable(False, False)
         self.gui()
         self.mainloop()
 
