585e35a8710182810b9442d0241d6886d14a7fdf
ColdWindScholar <3590361911@qq.com>
2025-08-20 23:07:28 +0800

    [Tools] update test script

--- src/c_module/c/t.py
+++ src/c_module/c/t.py
@@ -1,2 +1,4 @@
 import libutils
-libutils.ext4_extractor(".", "/metadata", "./metadata.img", "metadata", 4096, 'e', False)
+libutils.ext4_extractor(".", "/metadata", "./metadata.img", "metadata", 4096, 'e', False, "metadata")
+libutils.img2simg('raw.img', 'sparse.img', 4096, False)
+libutils.simg2img(['sparse.img'], 'raw.img')
