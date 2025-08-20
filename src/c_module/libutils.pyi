07c632c7b3897b1c3f41d54c6f12a3a64a1b136e
ColdWWindScholar <3590361911@qq.com>
2025-08-20 18:08:58 +0800

    [Tools] remove useless cde

---
+++ src/c_module/libutils.pyi
@@ -0,0 +1,17 @@
+#!/bin/env python3
+from typing import Literal
+
+
+def ext4_extractor(config_dir: str, mountpoint: str, filename: str, directory: str, blocksize: int,
+                   image_type: Literal['e', 's'], android_configure_only: bool) -> int:
+    """
+    :param config_dir: dir to store config files
+    :param mountpoint: e.g: "/system"
+    :param filename: image file path
+    :param directory: output directory
+    :param blocksize: recommended 4096 as default
+    :param image_type: if s, sparse. if e, raw.
+    :param android_configure_only: extract configure only
+    :return: 0 if successful else != 0
+    """
+    raise NotImplementedError("Sorry!Cannot use the func.")
