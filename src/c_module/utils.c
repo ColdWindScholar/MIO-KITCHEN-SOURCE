835cffe104d6bc13b194c08fd8356f5585277a4d
ColdWindScholar <3590361911@qq.com>
2025-08-30 12:15:05 +0800

    [Tools] add license

--- src/c_module/utils.c
+++ src/c_module/utils.c
@@ -1,3 +1,19 @@
+/*
+* Copyright (C) 2022-2025 The MIO-KITCHEN-SOURCE Project
+
+ Licensed under the GNU AFFERO GENERAL PUBLIC LICENSE, Version 3.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.gnu.org/licenses/agpl-3.0.en.html#license-text
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ C modules for MIO-KITCHEN
+ */
 #define _FILE_OFFSET_BITS 64
 #define _LARGEFILE64_SOURCE 1
 #include <Python.h>
