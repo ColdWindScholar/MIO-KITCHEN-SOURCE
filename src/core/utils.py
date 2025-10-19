865342218521f8e35e47b7d33fb772f8f3fefad5
ColdWindScholar <359036191@qq.com>
2025-10-19 13:28:23 +0800

    add squash support

--- src/core/utils.py
+++ src/core/utils.py
@@ -80,7 +80,7 @@ formats = ([b'PK', "zip"], [b'OPPOENCRYPT!', "ozip"], [b'7z', "7z"], [b'\x53\xef
            [b'\x89PNG', 'png'], [b"LOGO!!!!", 'logo', 4000], [b'\x28\xb5\x2f\xfd', 'zstd'],
            [b'(\x05\x00\x00$8"%', 'kdz'], [b"\x32\x96\x18\x74", 'dz'], [b'\xcf\xfa\xed\xfe', 'macos_bin'],
            [b'\xfa\xff\xfa\xff', 'pac', 2116],
-           [b"-rom1fs-", 'romfs'],[b'UBI#', "ubi"],[b"sqsh", "squashfs"], [b'hsqs', 'squashfs'],
+           [b"-rom1fs-", 'romfs'],[b'UBI#', "ubi"],[b"sqsh", "squashfs"], [b'hsqs', 'squashfs'],[b"\x85\x19","jffs2"],
            [b'###\x00|\x00\x00\x00LOGO_TABLE\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00P', 'guoke_logo']
            )
 
