c5c735d23fbc3fc33ce3681fd2b1f8e2bd3214b5
ColdWindScholar <3590361911@qq.com>
2025-11-09 03:49:51 +0800

    add squash support

---
+++ src/core/aml_image.py
@@ -0,0 +1,82 @@
+from ctypes import LittleEndianStructure, sizeof, memmove, string_at, addressof, byref, c_uint, c_ulonglong, c_char, \
+    c_ushort, c_uint64
+
+
+class BasicStruct(LittleEndianStructure):
+    @property
+    def _size(self):
+        return sizeof(type(self))
+
+    def __len__(self):
+        return self._size
+
+    def unpack(self, data: bytes):
+        if len(data) < self._size:
+            raise Exception("Input data size less than struct size.")
+        if not isinstance(data, (bytes, bytearray)):
+            raise Exception("Input data must be byte data or bytearray.")
+
+        return memmove(byref(self), data, self._size)
+
+    def pack(self):
+        return string_at(addressof(self), sizeof(self))
+
+
+amlogic_magic = 0x27b51956
+
+
+class VersionHeader(BasicStruct):
+    _fields_ = [
+        ("crc32", c_uint),
+        ("version", c_uint),
+    ]
+
+
+class AmlHeader(BasicStruct):
+    _pack_ = 1
+    _fields_ = [
+        ("crc32", c_uint),
+        ("version", c_uint),
+        # ---VersionHeader---
+        ("magic", c_uint),
+        ("imageSz", c_ulonglong),
+        ("itemAlginSize", c_uint),
+        ("itemNum", c_uint),
+        ("reserve", c_char * 44),
+    ]
+
+
+class ItemInfo(BasicStruct):
+    _pack_ = 1
+    _fields_ = [
+        ("itemId", c_uint),
+        ("fileType", c_uint),
+        ("curoffsetInItem", c_uint64),
+        ("offsetInImage", c_uint64),
+        ("itemMainType", c_char * 256),
+        ("itemSubType", c_char * 256),
+        ("verify", c_uint),
+        ("isBackUpItem", c_ushort),
+        ("backUpItemId", c_ushort),
+        ("reserve", c_char * 32),
+    ]
+
+def main(filepath:str, output_path:str):
+    with open(filepath, "rb") as f:
+        header = AmlHeader()
+        print(d := f.read(len(header)))
+        header.unpack(d)
+        print("header", header.itemNum)
+        if header.magic != amlogic_magic:
+            raise Exception("magic is not amlogic magic.")
+        i = 0
+        while i < header.itemNum:
+            h2 = ItemInfo()
+            h2.unpack(f.read(len(h2)))
+            print(h2.curoffsetInItem, h2.offsetInImage, bytes(h2.itemMainType).decode(), h2.itemSubType, h2.verify,
+                  h2.isBackUpItem)
+            print(f.tell())
+            i += 1
+
+if __name__ == "__main__":
+    main(r"C:\Users\16612\Downloads\晶晨线刷解压工具\bin\111.img")
---
+++ src/core/ntpi_unpacker.py
@@ -0,0 +1,6 @@
+
+class Main:
+    def __init__(self):
+        self.magic = b'NTPI'
+        self.support_minor_version = 1
+        self.suport_major_version = 1
