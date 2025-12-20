bba425f995b1845cb6eaf7988aa91715f47b699e
ColdWindScholar <3590361911@qq.com>
2025-12-21 01:15:19 +0800

    optimize some code

--- src/core/qsb_imger.py
+++ src/core/qsb_imger.py
@@ -8,7 +8,7 @@ from struct import unpack
 from xml.dom.minidom import parse
 
 
-def process_by_xml(file:str, prefix='system', out_file_path:str=None):
+def process_by_xml(file: str, prefix='system', out_file_path: str = None):
     dom = parse(os.path.join(file))
     elems_program = dom.getElementsByTagName('program')
     basedir = os.path.dirname(file)
@@ -20,7 +20,7 @@ def process_by_xml(file:str, prefix='system', out_file_path:str=None):
             n = int(file_name[file_name.find('_') + 1:file_name.find('.')])
             img_orders.append([n, i])
 
-    if len(img_orders) == 0:
+    if not len(img_orders):
         print(f'No match: {prefix}')
         return False
     img_orders.sort()
@@ -28,8 +28,7 @@ def process_by_xml(file:str, prefix='system', out_file_path:str=None):
     base_sector = 0
     total_sectors = 0
     for img_order in img_orders:
-        i = img_order[1]
-        elem = elems_program[i]
+        elem = elems_program[img_order[1]]
         file_name = elem.getAttribute('filename')
         file_sector_offset = int(elem.getAttribute('file_sector_offset'))
         start_sector = int(elem.getAttribute('start_sector'))
@@ -40,9 +39,8 @@ def process_by_xml(file:str, prefix='system', out_file_path:str=None):
         print(f'> {file_name}')
         print('>> reading...')
         try:
-            in_file = open(os.path.join(basedir, file_name), 'rb')
-            in_data = in_file.read()
-            in_file.close()
+            with open(os.path.join(basedir, file_name), 'rb') as in_file:
+                in_data = in_file.read()
         except IOError:
             print('<< failed!!!')
             return False
@@ -59,40 +57,34 @@ def process_by_xml(file:str, prefix='system', out_file_path:str=None):
                 print(f'<<< Total size(B): {total_size}')
             else:
                 print('<<< NOT ext4 img!!!')
-            if out_file_path is None:
-                out_filename = os.path.join(basedir, f'{prefix}.img')
-            else:
-                out_filename = os.path.join(out_file_path, f'{prefix}.img')
+            out_filename = os.path.join(basedir if out_file_path is None else out_file_path, f'{prefix}.img')
             out_file = open(out_filename, 'wb')
         fill_sectors = 0
         if 0 < cur_pos_sector < start_sector:
             fill_sectors = start_sector - cur_pos_sector
         fill_sectors += file_sector_offset
         if fill_sectors > 0:
-            print ('<< filling...')
+            print('<< filling...')
             out_file.write(fill_sectors * SECTOR_SIZE_IN_BYTES * b'\x00')
         print('<< writing...')
         out_file.write(in_data)
         out_file.flush()
         cur_pos_sector = start_sector + num_partition_sectors
-        print()
 
     fill_sectors = base_sector + total_sectors - cur_pos_sector
     if total_sectors > 0 and fill_sectors > 0:
-        print ('<< extra filling...')
+        print('<< extra filling...')
         out_file.write(fill_sectors * SECTOR_SIZE_IN_BYTES * b'\x00')
-        print ()
     out_file.close()
-    print (f'Finished: {out_filename}')
+    print(f'Finished: {out_filename}')
     return True
 
 
 def main():
-
     def usage(app):
-        print ('$: merge {prefix}_*.img\n@: 483E10D992F3776521A74B2F64AE2D37 (:\n#: 20161030 v1.0\n')
-        print (f'usage:\n{app} <rawprogram-xml-file> [img-prefix]')
-        print ('    img-prefix: system (default), factory, cache, userdata ...')
+        print('$: merge {prefix}_*.img\n@: 483E10D992F3776521A74B2F64AE2D37 (:\n#: 20161030 v1.0\n')
+        print(f'usage:\n{app} <rawprogram-xml-file> [img-prefix]')
+        print('    img-prefix: system (default), factory, cache, userdata ...')
 
     import sys
     if len(sys.argv) < 2:
