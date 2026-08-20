ca4a9119468c63c09df949ea46b8e97e8629450c
coldwindscholr <3590361911@qq.com>
2026-08-20 13:44:33 +0800

    update qtui

--- src/qtui/plugin.py
+++ src/qtui/plugin.py
@@ -1,6 +1,4 @@
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout
-from PySide6.QtCore import Qt
-from PySide6.QtGui import QPixmap
 from qfluentwidgets import (
     TitleLabel,
     PushButton,
