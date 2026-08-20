b84a379b73913b0f9c52260e4641c4b9db656218
coldwindscholr <3590361911@qq.com>
2026-08-21 02:30:29 +0800

    remove unused webui

--- src/tkui/tool.py
+++ src/tkui/tool.py
@@ -1525,15 +1525,7 @@ class Tool(TkinterEmbeddedPanel):
 
         self.notepad.pack(fill=BOTH, expand=True)
         self.rzf = ttk.Frame(self.sub_win3)
-        self.tsk = Label(self.sub_win3, text="MIO-KITCHEN", font=(None, 15))
         self.tsk.pack(padx=10, pady=10, side='top')
-
-        tr = ttk.LabelFrame(self.sub_win3, text=lang.text131)
-        tr2 = Label(tr, text=lang.text132 + '\n(pac ozip zip tar.md5 tar tar.gz kdz dz ops ofp ext4 erofs boot img)')
-        tr.bind('<Button-1>', lambda *x: dndfile([filedialog.askopenfilename()]))
-        tr.pack(padx=5, pady=5, side='top', fill=X)
-        tr2.bind('<Button-1>', lambda *x: dndfile([filedialog.askopenfilename()]))
-        tr2.pack(padx=5, pady=5, side='top', fill=X)
 
         self.rzf.pack(padx=5, pady=5, fill=BOTH, side=TOP)
         self.Clear_Load_canvas = Canvas(self.rzf)
