0795bea8be3f4f48769eb1697afd9d7c351a0b0c
ColdWindScholar <3590361911@qq.com>
2026-09-19 13:56:59 +0800

    change UI libs

--- build.py
+++ build.py
@@ -163,7 +163,6 @@ class Builder:
                 "--hidden-import", "win32api",
                 "--hidden-import", "win32com",
                 "--hidden-import", "win32",
-                "--hidden-import", "pythoncom",
                 '--splash',
                 'splash.png'
             ])
