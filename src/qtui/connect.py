487577c42c45bde8f873332884ffa995368600de
ColdWindScholar <3590361911@qq.com>
2026-09-25 11:28:30 +0800

    change UI libs

--- src/qtui/connect.py
+++ src/qtui/connect.py
@@ -82,11 +82,11 @@ def handle_disconnect():
 def handle_incoming_phone_action():
     auth_header = request.headers.get('Authorization', None)
     if not auth_header or not auth_header.startswith('MioKey'):
-        network_bridge.log_signal.emit("ERROR", f"Refused unauthenticated client request.")
+        network_bridge.log_signal.emit("WARN", f"Refused unauthenticated client request.")
         return "Unauthorized: Missing header", 401
 
     if ACTIVE_SESSION["token"] is None or auth_header[6:] != ACTIVE_SESSION["token"]:
-        network_bridge.log_signal.emit("ERROR", "Request dropped. Bad verification signature.")
+        network_bridge.log_signal.emit("WARN", "Request dropped. Bad verification signature.")
         return "Unauthorized: Invalid key token", 403
 
     network_bridge.log_signal.emit("INFO", f"Action request  authorized. Processing task...")
