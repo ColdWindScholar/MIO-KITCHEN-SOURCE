e836f34dd020dd8b0b865d7f565a1bcaf796a0e2
ColdWindScholar <3590361911@qq.com>
2026-09-25 15:38:10 +0800

    change UI libs

--- src/qtui/plugins.py
+++ src/qtui/plugins.py
@@ -989,38 +989,6 @@ class InstallMpk(MessageBoxBase):
         self.installb.setEnabled(False)
 
 
-class AppCard(CardWidget):
-
-    def __init__(self, icon, title: str, content: str, parent=None):
-        super().__init__(parent)
-        self.iconWidget = IconWidget(icon)
-        self.titleLabel = BodyLabel(title, self)
-        self.contentLabel = CaptionLabel(content, self)
-        self.openButton = PushButton(self.tr('Run'), self)
-        self.moreButton = TransparentDropDownToolButton(FluentIcon.MORE)
-
-        self.hBoxLayout = QHBoxLayout(self)
-        self.vBoxLayout = QVBoxLayout()
-
-        self.setFixedHeight(73)
-        self.iconWidget.setFixedSize(48, 48)
-        self.openButton.setFixedWidth(120)
-
-        self.hBoxLayout.setContentsMargins(20, 11, 11, 11)
-        self.hBoxLayout.setSpacing(15)
-        self.hBoxLayout.addWidget(self.iconWidget)
-
-        self.vBoxLayout.setContentsMargins(0, 0, 0, 0)
-        self.vBoxLayout.setSpacing(0)
-        self.vBoxLayout.addWidget(self.titleLabel, 0, Qt.AlignVCenter)
-        self.vBoxLayout.addWidget(self.contentLabel, 0, Qt.AlignVCenter)
-        self.vBoxLayout.setAlignment(Qt.AlignVCenter)
-        self.hBoxLayout.addLayout(self.vBoxLayout)
-
-        self.hBoxLayout.addStretch(1)
-        self.hBoxLayout.addWidget(self.openButton, 0, Qt.AlignRight)
-        self.hBoxLayout.addWidget(self.moreButton, 0, Qt.AlignRight)
-
 
 class AppCardRich(CardWidget):
 
