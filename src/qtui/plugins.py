80fddc3d7da55211caffbc1096da60aa61fde6bd
ColdWindScholar <3590361911@qq.com>
2026-09-25 16:19:12 +0800

    change UI libs

--- src/qtui/plugins.py
+++ src/qtui/plugins.py
@@ -1518,7 +1518,7 @@ class PluginPage(QWidget):
         card_widget.openButton.setDisabled(True)
         origin_text = card_widget.openButton.text()
         for file in files:
-            download_generator = utils.download_api(cfg.pluginRepo.value + file, temp, size_=size)
+            download_generator = utils.download_api(cfg.pluginRepo.value + file, temp, size_=size, chunk_size=size // 4)
             for percentage, speed_val, bytes_down, file_size_val, elapsed_val in download_generator:
                 card_widget.openButton.setText(f"{percentage} %")
             module_manager.install(os.path.join(temp, file))
