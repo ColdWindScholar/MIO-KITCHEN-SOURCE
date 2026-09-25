443f5b566126f90faf101508190f0e6e7ff4d30f
ColdWindScholar <3590361911@qq.com>
2026-09-25 08:30:32 +0800

    change UI libs

--- build.py
+++ build.py
@@ -91,7 +91,7 @@ class Builder:
         with open('bin/settings.json', 'r', encoding='utf-8') as f:
             ver = json.load(f)
             ver = ver['Tool']['Version']
-        for i in ['MIO-KITCHEN-win', 'MIO-KITCHEN-linux', 'MIO-KITCHEN-linux-aarch64.zip', 'MIO-KITCHEN-macos', 'MIO-KITCHEN-macos-intel']:
+        for i in ['MIO-KITCHEN-win', 'MIO-KITCHEN-linux', 'MIO-KITCHEN-linux-aarch64', 'MIO-KITCHEN-macos', 'MIO-KITCHEN-macos-intel']:
             name_list = i.rsplit('-')
             name_list.insert(2, ver)
             name = '-'.join(name_list)
