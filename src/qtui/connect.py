a9c8b1d458d3267ca83b540e053e1d0c6b429455
ColdWindScholar <3590361911@qq.com>
2026-09-27 01:36:39 +0800

    change UI libs

--- src/qtui/connect.py
+++ src/qtui/connect.py
@@ -101,7 +101,7 @@ def get_device_info():
     if ACTIVE_SESSION["token"] is None or auth_header[6:] != ACTIVE_SESSION["token"]:
         network_bridge.log_signal.emit("WARN", "Request dropped. Bad verification signature.")
         return "Unauthorized: Invalid key token", 403
-    json_text = json.dumps({"device_name": platform.node(), "system": system(), "machine": machine()},
+    json_text = json.dumps({"device_name": platform.node(), "system": system(), "machine": machine(), "kernel":  platform.release()},
                            ensure_ascii=True)
     return json_text, 200
 
