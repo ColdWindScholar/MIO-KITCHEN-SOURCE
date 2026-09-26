35ec6db0073f042a334c34bd32029b6a9ee95dec
ColdWindScholar <3590361911@qq.com>
2026-09-27 01:22:31 +0800

    change UI libs

--- src/qtui/connect.py
+++ src/qtui/connect.py
@@ -43,7 +43,7 @@ ACTIVE_SESSION = {
     "device_ip": None,
     "verify_code": v_code(4),
 }
-WS:Server | None = None
+WS: Server | None = None
 
 @flask_backend.route('/connect', methods=['POST'])
 def handle_handshake():
@@ -83,9 +83,11 @@ def handle_disconnect():
     ACTIVE_SESSION["device_ip"] = None
     network_bridge.connection_status_signal.emit(False, {})
     network_bridge.log_signal.emit("SUCCESS", f"Disconnected by {device_ip}")
+    global WS
     if WS:
         network_bridge.log_signal.emit("INFO", f"Close websocket connect.")
         WS.close(CloseReason.NORMAL_CLOSURE, message="User Disconnect.[Desktop]")
+        WS = None
 
     return "Disconnected already.", 200
 
